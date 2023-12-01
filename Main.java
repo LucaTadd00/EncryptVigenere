8374b49f30dfd0794bc45057cb2c812aecf3ef73
LucaTadd00 <ltytddi@gmail.com>
2023-12-01 14:37:33 +0000

    prima versione

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    Matrice matrix = new Matrice("quadranti");
+    Vigenere vig = new Vigenere(0,26,0,26, matrix);
+    Thread t = new Thread(vig);
+    t.start();
+    try {
+    t.join(); }
+    catch (InterruptedException e) {
+      System.out.println("bona");
+    }
+    matrix.stampa();
+    System.out.println("Cifriamo il nostro testo e trasformiamolo in un segreto!");
+  }
+}
